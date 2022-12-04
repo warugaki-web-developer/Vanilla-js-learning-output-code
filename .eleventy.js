7be130d3600f33af9bae7b7a3a90344e75f3c3ae
warugaki <warugaki.web.developer@gmail.com>
2022-12-04 13:39:38 +0900

    11tyの設定ファイルを作成

---
+++ .eleventy.js
@@ -0,0 +1,35 @@
+const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite');
+
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    tempFolderName: '.11ty-vite', // Default name of the temp folder
+    serverOptions: {},
+    viteOptions: {
+      clearScreen: false, // https://ja.vitejs.dev/config/shared-options.html#clearscreen
+      server: {
+        mode: 'development',
+        middlewareMode: true,
+      },
+      build: {
+        mode: 'production',
+      },
+    },
+  });
+
+  // Copy/pass-through files
+  eleventyConfig.addPassthroughCopy('src/assets/css');
+  eleventyConfig.addPassthroughCopy('src/assets/js');
+
+  return {
+    templateFormats: ['njk', 'html'],
+    htmlTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+    dir: {
+      input: 'src',
+      output: 'build',
+      includes: '_includes',
+      layouts: '_layouts',
+      data: '_data',
+    },
+  };
+};
